5c824cb485f6b347ec81ef4223be1ad7129dafe7
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2019-07-28 18:25:08 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Web Push
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/cretueusebiu/laravel-web-push-demo">github.com/cretueusebiu/laravel-web-push-demo</a>
                 </div>
             </div>
         </div>
